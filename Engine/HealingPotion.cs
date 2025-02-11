7d31d84a2705c4e2173460a149604b290634ed67
kuturin <agrabska1994@gmail.com>
2025-02-11 09:10:46 +0100

    Added new class, added parent class to HealingPotion, Weapon, Monster and Player Classes

--- Engine/HealingPotion.cs
+++ Engine/HealingPotion.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Engine
 {
-    public class HealingPotion
+    public class HealingPotion : Item
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string NamePlural { get; set; }
         public int AmountToHeal { get; set; }
     }
 }
