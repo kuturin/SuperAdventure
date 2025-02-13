b70a2279461311b9eb353519c0fb324990cfcf92
kuturin <agrabska1994@gmail.com>
2025-02-13 19:21:34 +0100

    Added new classes, added new properites and added new lines to the constructors

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -13,6 +13,7 @@ namespace Engine
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
         public Item RewardItem { get; set; }
+        public List<QuestCompletionItem> QuestCompletionItems { get; set; }
     }
     public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold)
         {
@@ -21,5 +22,6 @@ namespace Engine
             Description = description;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            QuestCompletionItems = new List<QuestCompletionItem>();
         }
     }
